aa738e80754cda189ca8f255bf3c02dca828bb8a
Valentin OMNES <vomnes@student.42.fr>
2018-02-12 20:32:29 +0100

    feat: Update db architetcure + .gitignore

--- server/lib/table_database.go
+++ server/lib/table_database.go
@@ -1,12 +1,14 @@
 package lib
 
-import (
-	"time"
-)
+import "time"
 
 // User is the data structure of the table User from PostgreSQL
 type User struct {
-	ID        int
-	Username  string
-	CreatedAt time.Time
+	ID        string    `db:"id"`
+	Username  string    `db:"username"`
+	Email     string    `db:"email"`
+	Lastname  string    `db:"lastname"`
+	Firstname string    `db:"firstname"`
+	Password  string    `db:"password"`
+	CreatedAt time.Time `db:"created_at"`
 }
--- server/setup_db.sql
+++ server/setup_db.sql
@@ -7,8 +7,12 @@ GRANT ALL PRIVILEGES ON DATABASE db_matcha TO vomnes;
 
 CREATE TABLE Users (
   ID SERIAL PRIMARY KEY,
-  username VARCHAR     NOT NULL,
+  username VARCHAR (65)     NOT NULL,
+  email VARCHAR (255) NOT NULL,
+  lastname VARCHAR (65)     NOT NULL,
+  firstname VARCHAR (65)     NOT NULL,
+  password VARCHAR (65)     NOT NULL,
   created_at timestamp with time zone DEFAULT current_timestamp
 );
 
-INSERT INTO Users (username) VALUES ('Valentin');
+INSERT INTO Users (username, email, lastname, firstname, password) VALUES ('vomnes', 'valentin.omnes@gmail.com', 'Omnes', 'Valentin', 'abc');
