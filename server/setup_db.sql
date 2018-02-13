17ac165a3befc1316fd24456158955b05ef96bf0
Valentin OMNES <vomnes@student.42.fr>
2018-02-13 11:55:28 +0100

    feat: Add db tests

--- server/setup_db.sql
+++ server/setup_db.sql
@@ -1,3 +1,4 @@
+\echo '------- Initialize db_matcha --------'
 DROP DATABASE IF EXISTS db_matcha;
 CREATE DATABASE db_matcha;
 
@@ -16,3 +17,22 @@ CREATE TABLE Users (
 );
 
 INSERT INTO Users (username, email, lastname, firstname, password) VALUES ('vomnes', 'valentin.omnes@gmail.com', 'Omnes', 'Valentin', 'abc');
+
+\echo '----- Initialize db_matcha_tests -----'
+
+DROP DATABASE IF EXISTS db_matcha_tests;
+CREATE DATABASE db_matcha_tests;
+
+GRANT ALL PRIVILEGES ON DATABASE db_matcha_tests TO vomnes;
+
+\connect db_matcha_tests;
+
+CREATE TABLE Users (
+  ID SERIAL PRIMARY KEY,
+  username VARCHAR (65)     NOT NULL,
+  email VARCHAR (255) NOT NULL,
+  lastname VARCHAR (65)     NOT NULL,
+  firstname VARCHAR (65)     NOT NULL,
+  password VARCHAR (65)     NOT NULL,
+  created_at timestamp with time zone DEFAULT current_timestamp
+);
