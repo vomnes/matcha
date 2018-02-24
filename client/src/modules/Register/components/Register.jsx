e3ca6357a4f5ecf0a3e172dd711767aad1c7e91e
Valentin Omnes <valentin.omnes@gmail.com>
2018-02-24 15:26:36 +0100

    [FRONT] feat: Improve front register

--- client/src/modules/Register/components/Register.jsx
+++ client/src/modules/Register/components/Register.jsx
@@ -5,33 +5,49 @@ class Register extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      name: '',
+      username: '',
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      rePassword: '',
+      newError: '',
     }
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleUserInput (e) {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({[name]: value});
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   }
-  handleSubmit(event) {
-    alert(event.username.value);
+  handleSubmit(e) {
+    console.log(this.state)
+    e.preventDefault();
   }
   render() {
     return (
       <div className="background">
-        <div id="register" class="card card-2">
+        <a href="/login"><p id="member">Already a member ?</p></a>
+        <div id="register" className="card card-2">
           <h2 className="title">Register</h2>
           <h3 className="sub-title">A simple dating website</h3>
           <form onSubmit={this.handleSubmit}>
-            <input class="input-form" id="placeholder-icon-username" placeholder="Username" type="text" name="username" value={this.state.name} onChange={(event) => this.handleUserInput(event)}/><br />
-            <input class="input-form" id="placeholder-icon-name" placeholder="First name" type="text" name="firstname"/><br />
-            <input class="input-form" id="placeholder-icon-name" placeholder="Last name" type="text" name="lastname"/><br />
-            <input class="input-form" id="placeholder-icon-email" placeholder="Email address" type="email" name="email"/><br />
-            <input class="input-form" id="placeholder-icon-password" placeholder="Password" type="password" name="password"/><br />
-            <input class="input-form" id="placeholder-icon-re-password" placeholder="Re-enter Password" type="password" name="re-password"/><br />
-            <input class="submit-form" type="submit" value="Register"/>
-            <p className="error">Content Error Content Error Content Error Content Error Content Error Content Error Content Error Content Error Content Error Content Error Content Error Content Error</p>
+            <input className="input-form" id="placeholder-icon-username" placeholder="Username" type="text" name="username"
+              value={this.state.username} onChange={this.handleUserInput}/><br />
+            <input className="input-form placeholder-icon-name" placeholder="First name" type="text" name="firstname"
+              value={this.state.firstname} onChange={this.handleUserInput}/><br />
+            <input className="input-form placeholder-icon-name" placeholder="Last name" type="text" name="lastname"
+              value={this.state.lastname} onChange={this.handleUserInput}/><br />
+            <input className="input-form" id="placeholder-icon-email" placeholder="Email address" type="email" name="email"
+              value={this.state.email} onChange={this.handleUserInput}/><br />
+            <input className="input-form" id="placeholder-icon-password" placeholder="Password" type="password" name="password"
+              value={this.state.password} onChange={this.handleUserInput}/><br />
+            <input className="input-form" id="placeholder-icon-re-password" placeholder="Re-enter Password" type="password" name="rePassword"
+              value={this.state.rePassword} onChange={this.handleUserInput}/><br />
+            <input className="submit-form" type="submit" value="Register"/>
+            <p className="error">{this.state.newError}</p>
           </form>
         </div>
       </div>
