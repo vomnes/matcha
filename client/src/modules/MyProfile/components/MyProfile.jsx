d066a5a81ef7a7f7ad9dee880f60945e74c97613
Valentin Omnes <valentin.omnes@gmail.com>
2018-03-02 00:34:16 +0100

    feat: Add upload profile picture

--- client/src/modules/MyProfile/components/MyProfile.jsx
+++ client/src/modules/MyProfile/components/MyProfile.jsx
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import './MyProfile.css';
 
+class EditPicture extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pictureVisible: false,
+    }
+    this.showEditPicture = this.showEditPicture.bind(this);
+    this.uploadPicture = this.uploadPicture.bind(this);
+  }
+  showEditPicture() {
+    this.setState({
+      pictureVisible: true,
+    })
+  }
+  uploadPicture() {
+    this.inputPicture.click();
+  }
+  render() {
+    var url = this.props.urlPicture
+    return (
+      <div className={this.props.className} onMouseOver={this.showEditPicture} onMouseOut={this.showEditPicture}>
+        {this.state.pictureVisible ? <span id="over" onClick={this.uploadPicture}>Change picture</span> : <span id="over">Change picture</span>}
+        <input type="hidden" style={{display:"none"}} name="MAX_FILE_SIZE" value="30000" />
+        <input ref={(input) => { this.inputPicture = input; }} id="upload-profile-picture-one" name="userfile" style={{display:"none"}} type="file" accept=".jpg, .jpeg, .png"/>
+        <div className="picture-background-profile" style={{ backgroundImage: "url(" + url + ")" }}></div>
+      </div>
+    )
+  }
+}
+
 class MyProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pictureVisible: {
-        one: false,
-      },
     }
-    this.showEditPicture = this.showEditPicture.bind(this);
-  }
-  showEditPicture() {
-    let pictureVisible = Object.assign({}, this.state.pictureVisible);
-    pictureVisible.one = !pictureVisible.one;
-    this.setState({
-      pictureVisible
-    })
   }
   render() {
     var profilePictures = [
@@ -24,28 +43,16 @@ class MyProfile extends Component {
       require('../../../design/pictures/Profile-molly-belle-73279-unsplash.jpg'),
       require('../../../design/pictures/Login-sorasak-217807-unsplash.jpg'),
     ];
-    let pictureVisible = Object.assign({}, this.state.pictureVisible);
     return (
       <div>
         <h1>My profile</h1>
         <div className="pictures">
-          <div className="profile-picture-one" onMouseOver={this.showEditPicture} onMouseOut={this.showEditPicture}>
-            {pictureVisible.one ? <span id="over">Change picture</span> : <span id="over">Change picture</span>}
-            <div className="picture-background-profile" style={{ backgroundImage: "url(" + profilePictures[0] + ")" }}></div>
-          </div>
+          <EditPicture className="profile-picture-one" urlPicture={profilePictures[2]} />
           <div className="picture-sub-area">
-            <div className="profile-picture-two">
-              <div className="picture-background-profile" style={{ backgroundImage: "url(" + profilePictures[2] + ")" }}></div>
-            </div>
-            <div className="profile-picture-three">
-              <div className="picture-background-profile" style={{ backgroundImage: "url(" + profilePictures[1] + ")" }}></div>
-            </div>
-            <div className="profile-picture-four">
-              <div className="picture-background-profile" style={{ backgroundImage: "url(" + profilePictures[2] + ")" }}></div>
-            </div>
-            <div className="profile-picture-five">
-              <div className="picture-background-profile" style={{ backgroundImage: "url(" + profilePictures[2] + ")" }}></div>
-            </div>
+            <EditPicture className="profile-picture-two" urlPicture={profilePictures[2]} />
+            <EditPicture className="profile-picture-three" urlPicture={profilePictures[1]} />
+            <EditPicture className="profile-picture-four" urlPicture={profilePictures[2]} />
+            <EditPicture className="profile-picture-five" urlPicture={profilePictures[2]} />
           </div>
         </div>
       </div>
