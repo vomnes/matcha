2fff6a26326a6014b89382c5726122583e03568f
Valentin Omnes <valentin.omnes@gmail.com>
2018-03-02 09:58:49 +0100

    feat: Add upload picture delete picture btn profile

--- client/src/modules/MyProfile/components/MyProfile.jsx
+++ client/src/modules/MyProfile/components/MyProfile.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './MyProfile.css';
 
+const ButtonEdit = (props) => {
+  var deleteBtn = null;
+  if (props.deleteAvailable) {
+    deleteBtn = <span className="over-delete" title="Delete picture" onClick={props.uploadPicture}>x</span>;
+  }
+  if (props.pictureVisible) {
+    return (
+      <div>
+        <span className="over-edit" onClick={props.uploadPicture} title="Change picture">Change picture</span>
+        {deleteBtn}
+      </div>
+    );
+  }
+  return null;
+}
+
 class EditPicture extends Component {
   constructor(props) {
     super(props);
@@ -8,11 +24,17 @@ class EditPicture extends Component {
       pictureVisible: false,
     }
     this.showEditPicture = this.showEditPicture.bind(this);
+    this.hideEditPicture = this.hideEditPicture.bind(this);
     this.uploadPicture = this.uploadPicture.bind(this);
   }
   showEditPicture() {
     this.setState({
       pictureVisible: true,
+    })
+  }
+  hideEditPicture() {
+    this.setState({
+      pictureVisible: false,
     })
   }
   uploadPicture() {
@@ -21,8 +43,12 @@ class EditPicture extends Component {
   render() {
     var url = this.props.urlPicture
     return (
-      <div className={this.props.className} onMouseOver={this.showEditPicture} onMouseOut={this.showEditPicture}>
-        {this.state.pictureVisible ? <span id="over" onClick={this.uploadPicture}>Change picture</span> : <span id="over">Change picture</span>}
+      <div className={this.props.className} onMouseEnter={this.showEditPicture} onMouseLeave={this.hideEditPicture}>
+        <ButtonEdit
+          pictureVisible={this.state.pictureVisible}
+          uploadPicture={this.uploadPicture}
+          deleteAvailable={this.props.deleteAvailable}
+        />
         <input type="hidden" style={{display:"none"}} name="MAX_FILE_SIZE" value="30000" />
         <input ref={(input) => { this.inputPicture = input; }} id="upload-profile-picture-one" name="userfile" style={{display:"none"}} type="file" accept=".jpg, .jpeg, .png"/>
         <div className="picture-background-profile" style={{ backgroundImage: "url(" + url + ")" }}></div>
@@ -49,10 +75,10 @@ class MyProfile extends Component {
         <div className="pictures">
           <EditPicture className="profile-picture-one" urlPicture={profilePictures[2]} />
           <div className="picture-sub-area">
-            <EditPicture className="profile-picture-two" urlPicture={profilePictures[2]} />
-            <EditPicture className="profile-picture-three" urlPicture={profilePictures[1]} />
-            <EditPicture className="profile-picture-four" urlPicture={profilePictures[2]} />
-            <EditPicture className="profile-picture-five" urlPicture={profilePictures[2]} />
+            <EditPicture className="profile-picture-two" urlPicture={profilePictures[2]} deleteAvailable="true" />
+            <EditPicture className="profile-picture-three" urlPicture={profilePictures[1]} deleteAvailable="true" />
+            <EditPicture className="profile-picture-four" urlPicture={profilePictures[2]} deleteAvailable="true" />
+            <EditPicture className="profile-picture-five" urlPicture={null} deleteAvailable="true" />
           </div>
         </div>
       </div>
