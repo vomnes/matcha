5fd96b001d230e73619b67cfd8e5589b28815863
Valentin OMNES <vomnes@student.42.fr>
2018-03-05 17:02:11 +0100

    feat: Update my profile edit fields

--- client/src/modules/MyProfile/components/MyProfile.jsx
+++ client/src/modules/MyProfile/components/MyProfile.jsx
@@ -115,7 +115,15 @@ class MyProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstname: 'mynameis',
+      firstname: '',
+      lastname: '',
+      email: '',
+      biography: '',
+      genre: 'male',
+      preference: 'female',
+      password: '',
+      rePassword: '',
+      birthday: '2018-03-09',
       variableModal: '',
     }
     this.handleUserInput = this.handleUserInput.bind(this);
@@ -124,8 +132,8 @@ class MyProfile extends Component {
     this.confirmDeletePicture = this.confirmDeletePicture.bind(this);
   }
   handleUserInput (e) {
-    const fieldName = e.target.name
-    var data = e.target.value
+    const fieldName = e.target.name;
+    var data = e.target.value;
     this.setState({
       [fieldName]: data,
     });
@@ -167,21 +175,54 @@ class MyProfile extends Component {
         <div className="myprofile-data">
           <div className="myprofile-id">
             <div className="fields-area">
+              <div className="limit" style={{ width: "10%" }}></div>
               <div className="field-title">
                 Edit your personal settings<br />
+                <span className="profile-username">@vomnes</span><br />
               </div>
-              <div className="limit" style={{ width: "10%" }}></div>
-              <span className="field-name">Username: </span>
-              <span>vomnes</span><br />
               <form>
-                <span className="field-name">Firstname:
+                <span className="field-name">Firstname :
                 <input className="field-input" placeholder="Valentin" type="text" name="firstname"
                   value={this.state.firstname} onChange={this.handleUserInput}/></span><br />
-                <span className="field-name">Lastname: </span><span>Omnes</span><br />
-                <span className="field-name">Email address: </span><span>valentin.omnes@gmail.com</span><br />
-                <span className="field-name">Biography: </span><span>Greatly hearted has who believe...</span><br />
-                <span className="field-name">Genre: </span><span></span><br />
-                <input type="submit" value="Update information"/>
+                <span className="field-name">Lastname :
+                <input className="field-input" placeholder="Omnes" type="text" name="lastname"
+                  value={this.state.lastname} onChange={this.handleUserInput}/></span><br />
+                <span className="field-name">Email address :
+                <input className="field-input" placeholder="valentin.omnes@gmail.com" type="text" name="email"
+                  value={this.state.email} onChange={this.handleUserInput}/></span><br />
+                <span className="field-name">Biography :
+                <input className="field-input" placeholder="Greatly hearted has who believe..." type="text" name="biography"
+                  value={this.state.biography} onChange={this.handleUserInput}/></span><br />
+                <span className="field-name">Birthday :
+                <input className="field-input" type="date" name="birthday"
+                  value={this.state.birthday} onChange={this.handleUserInput}/></span><br />
+                <input type="submit" value="Save"/>
+                <div className="limit" style={{ width: "10%" }}></div>
+                <span className="field-name">Genre :
+                  <select style={{ marginLeft: '5px' }} name="genre" value={this.state.genre} onChange={this.handleUserInput}>
+                    <option value="female">Female</option>
+                    <option value="male">Male</option>
+                  </select>
+                </span><br />
+                <span className="field-name">Interesting in :
+                  <select style={{ marginLeft: '5px' }} name="preference" value={this.state.preference} onChange={this.handleUserInput}>
+                    <option value="female">Female</option>
+                    <option value="male">Male</option>
+                    <option value="bisexual">Bisexual</option>
+                  </select>
+                </span><br />
+                <input type="submit" value="Save"/>
+                <div className="limit" style={{ width: "10%" }}></div>
+                <div className="field-title">Set your password<br /></div>
+                <span className="field-name">New password :
+                <input className="field-input" type="password" name="password"
+                  value={this.state.password} onChange={this.handleUserInput}/></span><br />
+                <span className="field-name">Type it again :
+                  <input className="field-input" type="password" name="rePassword"
+                    value={this.state.rePassword} onChange={this.handleUserInput}/></span><br />
+                <input type="submit" value="Update password"/>
+                <div className="limit" style={{ width: "10%" }}></div>
+                <div className="field-title">Update your tags<br /></div>
               </form>
             </div>
           </div>
