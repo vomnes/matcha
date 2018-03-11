f15b9a612f274d945409f5eab858f48044cf49d0
Valentin Omnes <valentin.omnes@gmail.com>
2018-03-12 00:22:49 +0100

    feat: Add upload picture in api

--- server/api/src/routes/profile/picture.go
+++ server/api/src/routes/profile/picture.go
@@ -1,11 +1,19 @@
 package profile
 
 import (
+	"bytes"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"../../../../lib"
-
 	"github.com/gorilla/mux"
 )
 
@@ -21,17 +29,110 @@ func checkInput(pictureNumber string) (int, string) {
 }
 
 type pictureData struct {
-	PictureBase64 string `json:"picture_base64"`
+	Base64 string `json:"picture_base64"`
+}
+
+func trimStringFromString(s, sub string) string {
+	if idx := strings.Index(s, sub); idx != -1 {
+		return s[:idx]
+	}
+	return s
+}
+
+func base64Decode(str string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(str)
+}
+
+func generatePng(path string, res io.Reader) (string, error) {
+	img, err := png.Decode(res)
+	if err != nil {
+		return "", err
+	}
+	f, err := os.OpenFile(path+".png", os.O_WRONLY|os.O_CREATE, 0777)
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	png.Encode(f, img)
+	return path + ".png", nil
+}
+
+func generateJpeg(path string, res io.Reader) (string, error) {
+	img, err := png.Decode(res)
+	if err != nil {
+		return "", err
+	}
+	f, err := os.OpenFile(path+".jpeg", os.O_WRONLY|os.O_CREATE, 0777)
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	jpeg.Encode(f, img, &jpeg.Options{Quality: 100})
+	return path + ".jpeg", nil
+}
+
+func base64ToImageFile(base64 string, pictureNumber, username string) (string, int, string, error) {
+	path, err := os.Getwd()
+	if err != nil {
+		return "", 500, "Failed to get the root path name - EncodeBase64", err
+	}
+	path = strings.TrimSuffix(path, "/api/src/routes/profile")
+	subPath := "/storage/pictures/profiles/"
+	if username == "test" {
+		subPath = "/storage/tests/"
+	}
+	newpath := path + subPath + username
+	os.MkdirAll(newpath, os.ModePerm)
+	fileName := lib.GetRandomString(43) + "_" + pictureNumber
+	preBase64 := trimStringFromString(base64, ";base64")
+	typeImage := string(preBase64)[5:]
+	imageBase64 := string(base64)[len(preBase64)+8:]
+	unbased, _ := base64Decode(imageBase64)
+	res := bytes.NewReader(unbased)
+	var imagePath string
+	switch typeImage {
+	case "image/png":
+		imagePath, err = generatePng(newpath+"/"+fileName, res)
+		if err != nil {
+			log.Println(lib.PrettyError("[Base 64] Failed to generate png file - " + err.Error()))
+			return "", 500, "Failed to generate png file", err
+		}
+	case "image/jpg":
+		imagePath, err = generateJpeg(newpath+"/"+fileName, res)
+		if err != nil {
+			log.Println(lib.PrettyError("[Base 64] Failed to generate jpg file - " + err.Error()))
+			return "", 500, "Failed to generate jpg file", err
+		}
+	case "image/jpeg":
+		imagePath, err = generateJpeg(newpath+"/"+fileName, res)
+		if err != nil {
+			log.Println(lib.PrettyError("[Base 64] Failed to generate jpeg file - " + err.Error()))
+			return "", 500, "Failed to generate jpeg file", err
+		}
+	default:
+		return "", 406, "Image type [" + typeImage + "] not accepted, support only png, jpg and jpeg images", errors.New("Unsupported file type")
+	}
+	return imagePath, 0, "", nil
 }
 
 func uploadPicture(w http.ResponseWriter, r *http.Request, pictureNumber string) {
+	username, ok := r.Context().Value(lib.Username).(string)
+	if !ok {
+		lib.RespondWithErrorHTTP(w, 500, "Failed to collect username")
+		return
+	}
 	var inputData pictureData
 	errCode, errContent, err := lib.GetDataBody(r, &inputData)
 	if err != nil {
 		lib.RespondWithErrorHTTP(w, errCode, errContent)
 		return
 	}
-	fmt.Println(pictureNumber, " POST")
+	imagePath, errCode, errContent, err := base64ToImageFile(inputData.Base64, pictureNumber, username)
+	if err != nil {
+		lib.RespondWithErrorHTTP(w, errCode, errContent)
+		return
+	}
+	fmt.Println(imagePath)
 	lib.RespondEmptyHTTP(w, http.StatusOK)
 }
 
--- server/api/src/routes/profile/picture_test.go
+++ server/api/src/routes/profile/picture_test.go
@@ -49,18 +49,115 @@ func TestPictureInvalidURLParameter(t *testing.T) {
 	}
 }
 
-func TestPictureUpload(t *testing.T) {
+func TestPictureUploadFailedToDecodeBody(t *testing.T) {
 	tests.DbClean()
 	context := tests.ContextData{
 		DB: tests.DB,
 	}
-	body := []byte(`{"picture_base64": "v@"}`)
+	body := []byte(`{"picture_base64": }`)
 	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
 	r.Header.Add("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 	testApplicantServer().ServeHTTP(w, r)
-	strError := tests.CompareResponseJSONCode(w, 200, map[string]interface{}{})
+	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
+		"error": "Failed to decode body",
+	})
 	if strError != nil {
 		t.Errorf("%v", strError)
 	}
 }
+
+func TestPictureUploadFailToGeneratePng(t *testing.T) {
+	tests.DbClean()
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: "test",
+	}
+	body := []byte(`{"picture_base64": "data:image/png;base64,iVBORw0KGgoAAAANS"}`)
+	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	testApplicantServer().ServeHTTP(w, r)
+	strError := tests.CompareResponseJSONCode(w, 500, map[string]interface{}{
+		"error": "Failed to generate png file",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestPictureUploadFailToGenerateJpg(t *testing.T) {
+	tests.DbClean()
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: "test",
+	}
+	body := []byte(`{"picture_base64": "data:image/jpg;base64,iVBORw0KGgoAAAANS"}`)
+	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	testApplicantServer().ServeHTTP(w, r)
+	strError := tests.CompareResponseJSONCode(w, 500, map[string]interface{}{
+		"error": "Failed to generate jpg file",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestPictureUploadFailToGenerateJpeg(t *testing.T) {
+	tests.DbClean()
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: "test",
+	}
+	body := []byte(`{"picture_base64": "data:image/jpeg;base64,iVBORw0KGgoAAAANS"}`)
+	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	testApplicantServer().ServeHTTP(w, r)
+	strError := tests.CompareResponseJSONCode(w, 500, map[string]interface{}{
+		"error": "Failed to generate jpeg file",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestPictureUploadBody(t *testing.T) {
+	tests.DbClean()
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: "test",
+	}
+	body := []byte(`{"picture_base64": "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfiAwcPEjOBVwS4AAAA70lEQVQoz13RQUoCYRjG8Z+mgxpCQTnSwgu4a9mqaBHkppauPIoH6BJ2gg7hDVq5KwoGFYIiUpiSafHN5OT/3b38eR6+96OgYirLZ6qyXRfE5kZe0HOvaxHWNVduQQvX3nGAOyvwsKfvwlAiM5NpaGCm6shQ4hGOZfp2OZVpU8WbjVhk7MSNGz1jkdjaZ2HPDUUyZyYmzmUiI8+EBBZiqY0maNpIdcI7grAUY62VC2vElrvCKhcaVuiUhUUuFBVB+FfRLSUEIa+olRISPz7wLdkmBAbSnTNVpC63CUt1A18lYV89VAQOvf59dTFP2vALI8pQKcosrXkAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDMtMDdUMTU6MTg6NTErMDE6MDCZx6j3AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTAzLTA3VDE1OjE4OjUxKzAxOjAw6JoQSwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII="}`)
+	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	testApplicantServer().ServeHTTP(w, r)
+	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
+		"error": "Image type [image/gif] not accepted, support only png, jpg and jpeg images",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+// func TestPictureUpload(t *testing.T) {
+// 	tests.DbClean()
+// 	context := tests.ContextData{
+// 		DB:       tests.DB,
+// 		Username: "test",
+// 	}
+// 	body := []byte(`{"picture_base64": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfiAwcPEjOBVwS4AAAA70lEQVQoz13RQUoCYRjG8Z+mgxpCQTnSwgu4a9mqaBHkppauPIoH6BJ2gg7hDVq5KwoGFYIiUpiSafHN5OT/3b38eR6+96OgYirLZ6qyXRfE5kZe0HOvaxHWNVduQQvX3nGAOyvwsKfvwlAiM5NpaGCm6shQ4hGOZfp2OZVpU8WbjVhk7MSNGz1jkdjaZ2HPDUUyZyYmzmUiI8+EBBZiqY0maNpIdcI7grAUY62VC2vElrvCKhcaVuiUhUUuFBVB+FfRLSUEIa+olRISPz7wLdkmBAbSnTNVpC63CUt1A18lYV89VAQOvf59dTFP2vALI8pQKcosrXkAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDMtMDdUMTU6MTg6NTErMDE6MDCZx6j3AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTAzLTA3VDE1OjE4OjUxKzAxOjAw6JoQSwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII="}`)
+// 	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
+// 	r.Header.Add("Content-Type", "application/json")
+// 	w := httptest.NewRecorder()
+// 	testApplicantServer().ServeHTTP(w, r)
+// 	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
+// 		"error": "Failed to decode body",
+// 	})
+// 	if strError != nil {
+// 		t.Errorf("%v", strError)
+// 	}
+// }
