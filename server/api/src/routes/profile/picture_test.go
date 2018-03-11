804f1a37173d0fdee56717c89db51fb8da5f579b
Valentin Omnes <valentin.omnes@gmail.com>
2018-03-12 00:46:36 +0100

    test: Add check if file image has been created

--- server/api/src/routes/profile/picture_test.go
+++ server/api/src/routes/profile/picture_test.go
@@ -1,8 +1,11 @@
 package profile
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"../../../../tests"
@@ -143,21 +146,49 @@ func TestPictureUploadBody(t *testing.T) {
 	}
 }
 
-// func TestPictureUpload(t *testing.T) {
-// 	tests.DbClean()
-// 	context := tests.ContextData{
-// 		DB:       tests.DB,
-// 		Username: "test",
-// 	}
-// 	body := []byte(`{"picture_base64": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfiAwcPEjOBVwS4AAAA70lEQVQoz13RQUoCYRjG8Z+mgxpCQTnSwgu4a9mqaBHkppauPIoH6BJ2gg7hDVq5KwoGFYIiUpiSafHN5OT/3b38eR6+96OgYirLZ6qyXRfE5kZe0HOvaxHWNVduQQvX3nGAOyvwsKfvwlAiM5NpaGCm6shQ4hGOZfp2OZVpU8WbjVhk7MSNGz1jkdjaZ2HPDUUyZyYmzmUiI8+EBBZiqY0maNpIdcI7grAUY62VC2vElrvCKhcaVuiUhUUuFBVB+FfRLSUEIa+olRISPz7wLdkmBAbSnTNVpC63CUt1A18lYV89VAQOvf59dTFP2vALI8pQKcosrXkAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDMtMDdUMTU6MTg6NTErMDE6MDCZx6j3AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTAzLTA3VDE1OjE4OjUxKzAxOjAw6JoQSwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII="}`)
-// 	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
-// 	r.Header.Add("Content-Type", "application/json")
-// 	w := httptest.NewRecorder()
-// 	testApplicantServer().ServeHTTP(w, r)
-// 	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
-// 		"error": "Failed to decode body",
-// 	})
-// 	if strError != nil {
-// 		t.Errorf("%v", strError)
-// 	}
-// }
+func getPathNameTest(t *testing.T) string {
+	path, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+	return strings.TrimSuffix(path, "/api/src/routes/profile")
+}
+
+func isEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+	_, err = f.Readdirnames(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
+}
+
+func TestPictureUpload(t *testing.T) {
+	tests.DbClean()
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: "test",
+	}
+	body := []byte(`{"picture_base64": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfiAwcPEjOBVwS4AAAA70lEQVQoz13RQUoCYRjG8Z+mgxpCQTnSwgu4a9mqaBHkppauPIoH6BJ2gg7hDVq5KwoGFYIiUpiSafHN5OT/3b38eR6+96OgYirLZ6qyXRfE5kZe0HOvaxHWNVduQQvX3nGAOyvwsKfvwlAiM5NpaGCm6shQ4hGOZfp2OZVpU8WbjVhk7MSNGz1jkdjaZ2HPDUUyZyYmzmUiI8+EBBZiqY0maNpIdcI7grAUY62VC2vElrvCKhcaVuiUhUUuFBVB+FfRLSUEIa+olRISPz7wLdkmBAbSnTNVpC63CUt1A18lYV89VAQOvf59dTFP2vALI8pQKcosrXkAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTgtMDMtMDdUMTU6MTg6NTErMDE6MDCZx6j3AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE4LTAzLTA3VDE1OjE4OjUxKzAxOjAw6JoQSwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAASUVORK5CYII="}`)
+	r := tests.CreateRequest("POST", "/v1/profiles/picture/"+"1", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	testApplicantServer().ServeHTTP(w, r)
+	path := getPathNameTest(t)
+	empty, err := isEmpty(path + "/storage/tests/test")
+	if err != nil {
+		t.Error(err)
+	}
+	if empty {
+		t.Error("Directory must not be empty, the file hasn't been created")
+	}
+	os.RemoveAll(path + "/storage/tests")
+	strError := tests.CompareResponseJSONCode(w, 200, map[string]interface{}{})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
