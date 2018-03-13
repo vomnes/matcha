02b2c68a644adfd79370ee25d6995c5c4962b0b2
Valentin Omnes <valentin.omnes@gmail.com>
2018-03-13 16:33:49 +0100

    feat: Add profile/edit/password with tests

--- server/api/src/routes/profile/editPassword.go
+++ server/api/src/routes/profile/editPassword.go
@@ -1,10 +1,13 @@
 package profile
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 	"net/http"
 
 	"../../../../lib"
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type userPassword struct {
@@ -25,8 +28,40 @@ func checkInputBody(inputData userPassword) (int, string) {
 		return 406, "Both password entered must be identical"
 	}
 	if !lib.IsValidPassword(inputData.NewPassword) {
-		return 406, "Not a valid password"
+		return 406, "Not a valid new password"
 	}
+	return 0, ""
+}
+
+func checkCurrentUserPassword(r *http.Request, db *sqlx.DB, password, userId, username string) (int, string) {
+	var user lib.User
+	err := db.Get(&user, "SELECT * FROM Users WHERE id = $1 AND username = $2", userId, username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return 400, "User does not exists in the database"
+		}
+		log.Println(lib.PrettyError(r.URL.String() + " [DB REQUEST - SELECT] " + err.Error()))
+		return 500, "Failed to check if users exists in the database"
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return 403, "Current password incorrect"
+	}
+	return 0, ""
+}
+
+func updateUserPassword(r *http.Request, db *sqlx.DB, password, userId, username string) (int, string) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(lib.PrettyError(r.URL.String() + " [PW - BCRYPT] " + err.Error()))
+		return 500, "Password encryption failed"
+	}
+	stmt, err := db.Preparex(`UPDATE users SET password = $1 WHERE id = $2 AND username = $3`)
+	if err != nil {
+		log.Fatal(lib.PrettyError(r.URL.String() + "[DB REQUEST - INSERT] Failed to prepare request update user" + err.Error()))
+		return 500, "Prepare SQL request failed"
+	}
+	_ = stmt.QueryRow(hashedPassword, userId, username)
 	return 0, ""
 }
 
@@ -47,5 +82,14 @@ func EditPassword(w http.ResponseWriter, r *http.Request) {
 		lib.RespondWithErrorHTTP(w, errCode, errContent)
 		return
 	}
-	fmt.Println(db, username, userId)
+	errCode, errContent = checkCurrentUserPassword(r, db, inputData.CurrentPassword, userId, username)
+	if errCode != 0 || errContent != "" {
+		lib.RespondWithErrorHTTP(w, errCode, errContent)
+		return
+	}
+	errCode, errContent = updateUserPassword(r, db, inputData.NewPassword, userId, username)
+	if errCode != 0 || errContent != "" {
+		lib.RespondWithErrorHTTP(w, errCode, errContent)
+		return
+	}
 }
--- server/api/src/routes/profile/editPassword_test.go
+++ server/api/src/routes/profile/editPassword_test.go
@@ -72,6 +72,205 @@ func TestEditPasswordEmptyFields(t *testing.T) {
 	}
 }
 
+func TestEditPasswordInvalidCurrentPassword(t *testing.T) {
+	tests.DbClean()
+	username := "test_" + lib.GetRandomString(43)
+	userData := tests.InsertUser(lib.User{Username: username, Password: "HelloWorld"}, tests.DB)
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: username,
+		UserID:   userData.ID,
+	}
+	body := []byte(`{
+    "password": "abc",
+    "new_password": "notNull",
+    "new_rePassword": "notNull"
+    }`)
+	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	output := tests.CaptureOutput(func() {
+		EditPassword(w, r)
+	})
+	// Check : Content stardard output
+	if output != "" {
+		t.Error(output)
+	}
+	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
+		"error": "Current password field is not a valid password",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestEditPasswordInvalidNewPassword(t *testing.T) {
+	tests.DbClean()
+	username := "test_" + lib.GetRandomString(43)
+	userData := tests.InsertUser(lib.User{Username: username, Password: "HelloWorld"}, tests.DB)
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: username,
+		UserID:   userData.ID,
+	}
+	body := []byte(`{
+    "password": "abcABC123",
+    "new_password": "notNull",
+    "new_rePassword": "notNull"
+    }`)
+	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	output := tests.CaptureOutput(func() {
+		EditPassword(w, r)
+	})
+	// Check : Content stardard output
+	if output != "" {
+		t.Error(output)
+	}
+	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
+		"error": "Not a valid new password",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestEditPasswordTwoNewPasswordInputMustBeIdentical(t *testing.T) {
+	tests.DbClean()
+	username := "test_" + lib.GetRandomString(43)
+	userData := tests.InsertUser(lib.User{Username: username, Password: "HelloWorld"}, tests.DB)
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: username,
+		UserID:   userData.ID,
+	}
+	body := []byte(`{
+    "password": "abcABC123",
+    "new_password": "abcABC123",
+    "new_rePassword": "abcABC123c"
+    }`)
+	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	output := tests.CaptureOutput(func() {
+		EditPassword(w, r)
+	})
+	// Check : Content stardard output
+	if output != "" {
+		t.Error(output)
+	}
+	strError := tests.CompareResponseJSONCode(w, 406, map[string]interface{}{
+		"error": "Both password entered must be identical",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestEditPasswordWrongPassword(t *testing.T) {
+	tests.DbClean()
+	username := "test_" + lib.GetRandomString(43)
+	userData := tests.InsertUser(lib.User{Username: username}, tests.DB)
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: username,
+		UserID:   userData.ID,
+	}
+	body := []byte(`{
+    "password": "abcABC123",
+    "new_password": "abcABC1232019",
+    "new_rePassword": "abcABC1232019"
+    }`)
+	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	output := tests.CaptureOutput(func() {
+		EditPassword(w, r)
+	})
+	// Check : Content stardard output
+	if output != "" {
+		t.Error(output)
+	}
+	strError := tests.CompareResponseJSONCode(w, 403, map[string]interface{}{
+		"error": "Current password incorrect",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+func TestEditPasswordUserDoesntExits(t *testing.T) {
+	tests.DbClean()
+	username := "test_" + lib.GetRandomString(43)
+	_ = tests.InsertUser(lib.User{Username: username}, tests.DB)
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: username,
+		UserID:   "42",
+	}
+	body := []byte(`{
+    "password": "abcABC123",
+    "new_password": "abcABC1232019",
+    "new_rePassword": "abcABC1232019"
+    }`)
+	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	output := tests.CaptureOutput(func() {
+		EditPassword(w, r)
+	})
+	// Check : Content stardard output
+	if output != "" {
+		t.Error(output)
+	}
+	strError := tests.CompareResponseJSONCode(w, 400, map[string]interface{}{
+		"error": "User does not exists in the database",
+	})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+}
+
+// abcABC123 -> $2a$10$pgek6WtdhtKmGXPWOOtEf.gsgtNXOkqr3pBjaCCa9il6XhRS7LAua
+func TestEditPassword(t *testing.T) {
+	tests.DbClean()
+	username := "test_" + lib.GetRandomString(43)
+	userData := tests.InsertUser(lib.User{Username: username, Password: "$2a$10$pgek6WtdhtKmGXPWOOtEf.gsgtNXOkqr3pBjaCCa9il6XhRS7LAua"}, tests.DB)
+	context := tests.ContextData{
+		DB:       tests.DB,
+		Username: username,
+		UserID:   userData.ID,
+	}
+	body := []byte(`{
+    "password": "abcABC123",
+    "new_password": "abcABC1232019",
+    "new_rePassword": "abcABC1232019"
+    }`)
+	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
+	r.Header.Add("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	output := tests.CaptureOutput(func() {
+		EditPassword(w, r)
+	})
+	// Check : Content stardard output
+	if output != "" {
+		t.Error(output)
+	}
+	strError := tests.CompareResponseJSONCode(w, 200, map[string]interface{}{})
+	if strError != nil {
+		t.Errorf("%v", strError)
+	}
+	// Check : Updated data in database
+	var user lib.User
+	err := tests.DB.Get(&user, "SELECT id, username, password FROM Users WHERE username = $1", username)
+	if err != nil {
+		t.Error("\x1b[1;31m" + err.Error() + "\033[0m")
+		return
+	}
+	// Check password -->
+}
+
 // func TestEditPassword(t *testing.T) {
 // 	tests.DbClean()
 // 	today := time.Now()
@@ -87,7 +286,7 @@ func TestEditPasswordEmptyFields(t *testing.T) {
 // 	body := []byte(`{
 //     "password": "",
 //     "new_password": "",
-//     "new_rePassword": "",
+//     "new_rePassword": ""
 //     }`)
 // 	r := tests.CreateRequest("POST", "/v1/profiles/edit/password", body, context)
 // 	r.Header.Add("Content-Type", "application/json")
