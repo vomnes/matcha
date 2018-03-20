4f31fa21ae1bbf549f64caed2e54a9445dcc1cd2
Valentin OMNES <vomnes@student.42.fr>
2018-03-20 18:17:10 +0100

    feat: Add tags request in route match

--- server/api/src/routes/user/match.go
+++ server/api/src/routes/user/match.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"../../../../lib"
 	"github.com/jmoiron/sqlx"
@@ -12,24 +13,24 @@ import (
 )
 
 type match struct {
-	ID            string   `db:"id"`
-	Username      string   `db:"username"`
-	Lastname      string   `db:"lastname"`
-	Firstname     string   `db:"firstname"`
-	PictureURL1   string   `db:"picture_url_1"`
-	Genre         string   `db:"genre"`
-	InterestingIn string   `db:"interesting_in"`
-	Latitude      *float64 `db:"latitude"`
-	Longitude     *float64 `db:"longitude"`
-	Distance      *float64 `db:"distance"`
-	Age           string   `db:"age"`
+	ID          string   `db:"id"`
+	Username    string   `db:"username"`
+	Lastname    string   `db:"lastname"`
+	Firstname   string   `db:"firstname"`
+	PictureURL1 string   `db:"picture_url_1"`
+	Latitude    *float64 `db:"latitude"`
+	Longitude   *float64 `db:"longitude"`
+	Distance    *float64 `db:"distance"`
+	Age         string   `db:"age"`
+	CommonTags  int      `db:"common_tags"`
+	Rating      *float64 `db:"rating"`
 }
 
 func getLoggedInUserData(db *sqlx.DB, userID string) (lib.User, int, string) {
 	var loggedInUser lib.User
 	err := db.Get(&loggedInUser, `SELECT id, genre, interesting_in FROM Users WHERE id = $1`, userID)
 	if err != nil {
-		log.Println(lib.PrettyError("[DB REQUEST - SELECT] Failed to collect user data in database" + err.Error()))
+		log.Println(lib.PrettyError("[DB REQUEST - SELECT] Failed to collect user data in database " + err.Error()))
 		return lib.User{}, 500, "Failed to collect user data in the database"
 	}
 	return loggedInUser, 0, ""
@@ -48,9 +49,9 @@ func handleGenre(loggedInUser lib.User) (string, string) {
 
 func blockedRequest(one string) string {
 	return `
-  id NOT IN (Select target_userid From Fake_Reports Where userid = ` + one + `)
+  u.id NOT IN (Select target_userid From Fake_Reports Where userid = ` + one + `)
     AND
-  id NOT IN (Select userid From Fake_Reports Where target_userid = ` + one + `)
+  u.id NOT IN (Select userid From Fake_Reports Where target_userid = ` + one + `)
   `
 }
 
@@ -59,28 +60,29 @@ func getUsers(db *sqlx.DB, userID string) ([]match, int, string) {
 	if errCode != 0 && errContent != "" {
 		return []match{}, errCode, errContent
 	}
+	userTagsIds := []string{"1", "2"}
 	matchGenre, matchInterestingIn := handleGenre(loggedInUser)
 	var users []match
-	request := `SELECT
-	  id, username, latitude, longitude,
-	  geodistance(latitude, longitude, $1, $2) as distance,
-	  ageyear(birthday) as age
-	  FROM Users
+	request := `SELECT u.id, u.username, u.latitude, u.longitude,
+		(Select COUNT(*) from users_tags Where userid = u.id AND tagid IN (` + strings.Join(userTagsIds, ", ") + `)) as common_tags,
+	  geodistance(u.latitude, u.longitude, $1, $2) as distance,
+	  ageyear(u.birthday) as age,
+		u.rating
+	  FROM Users u
 	  WHERE
-	    id <> $3 AND
-	    genre IN (` + matchGenre + `) AND
-	    interesting_in IN (` + matchInterestingIn + `) AND
+	    u.id <> $3 AND
+	    u.genre IN (` + matchGenre + `) AND
+	    u.interesting_in IN (` + matchInterestingIn + `) AND
 	    ` + blockedRequest("$3")
+	fmt.Println(request)
 	err := db.Select(&users, request, 48.8895812, 2.3393303, userID)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			fmt.Println("Here")
 			return []match{}, 0, ""
 		}
-		log.Println(lib.PrettyError("[DB REQUEST - SELECT] Failed to collect user data in database" + err.Error()))
+		log.Println(lib.PrettyError("[DB REQUEST - SELECT] Failed to collect user data in database " + err.Error()))
 		return []match{}, 500, "Failed to collect user data in the database"
 	}
-	fmt.Println("-->", users)
 	return users, 0, ""
 }
 
