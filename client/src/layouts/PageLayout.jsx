11b1b501619bf19a0f463e1d704bba441a68608e
Valentin OMNES <vomnes@student.42.fr>
2018-04-11 15:03:53 +0200

    feat: Add case notifications

--- client/src/layouts/PageLayout.jsx
+++ client/src/layouts/PageLayout.jsx
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
 import './PageLayout.css';
+import ViewIcon from '../design/icons/eye.svg'
+import HeartIcon from '../design/icons/like.svg'
+import BrokenHeartIcon from '../design/icons/broken-heart-divided-in-two-parts.svg'
+import MatchIcon from '../design/icons/flame.svg'
+import MessageIcon from '../design/icons/conversation-speech-bubbles.svg'
+
+const NotifElement = (props) => {
+  var icon, content;
+  switch (props.type) {
+    case "view":
+        icon = ViewIcon;
+        content = `${props.fullname} has viewed your profile`;
+        break;
+    case "like":
+        icon = HeartIcon;
+        content = `${props.fullname} has liked your profile`;
+        break;
+    case "unlike":
+        icon = BrokenHeartIcon;
+        content = `${props.fullname} has unliked your profile`;
+        break;
+    case "match":
+        icon = MatchIcon;
+        content = `You have a match with ${props.fullname}`;
+        break;
+    case "message":
+        icon = MessageIcon;
+        content = `${props.fullname} has sent you a message`;
+        break;
+    default:
+        icon = ViewIcon;
+        content = '';
+  }
+  return (
+    <div className="notif-element">
+      <div className="picture-notif-background" style={{ backgroundImage: `url(${props.picture})` }}></div>
+      <div className="white-notif-background"></div>
+      <div className="notif-logo" title="See more">
+        <img alt="View profile" src={icon} style={{ width: "100%", opacity: "0.7" }}/>
+      </div>
+      <span className="notif-text">{content}<br/>{props.date}</span>
+    </div>
+  )
+}
 
 const NotifList = (props) => {
   return (
     <div id="notif">
       <div id="notif-arrow"></div>
-      <div className="notif-element">
-        <div className="picture-notif-background" style={{ backgroundImage: "url(https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10)" }}></div>
-        <div className="white-notif-background"></div>
-      </div>
-      <div className="notif-element">
-        <div className="picture-notif-background" style={{ backgroundImage: "url(https://images.unsplash.com/photo-1522234811749-abc512463137?h=1000&q=10)" }}></div>
-        <div className="white-notif-background"></div>
-      </div>
+      <NotifElement type="unlike" fullname="Valentin Omnes" picture="https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10" date={"12/12/12 - 12:42"}/>
+      <NotifElement type="message" fullname="Valentin Omnes" picture="https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10" date={"12/12/12 - 12:42"}/>
+      <NotifElement type="match" fullname="Valentin Omnes" picture="https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10" date={"12/12/12 - 12:42"}/>
+      <NotifElement type="like" fullname="Valentin Omnes" picture="https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10" date={"12/12/12 - 12:42"}/>
+      <NotifElement type="view" fullname="Valentin Omnes" picture="https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10" date={"12/12/12 - 12:42"}/>
     </div>
   )
 }
+
+// -> visit
+// -> like
+// -> unlike
+// -> match
+// -> message
 
 class PageLayout extends Component {
   render() {
