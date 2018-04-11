40f16fca80327c3ce1329325d0bfd6d554833f18
Valentin OMNES <vomnes@student.42.fr>
2018-04-11 13:31:42 +0200

    feat: Start/Add notif

--- client/src/layouts/PageLayout.jsx
+++ client/src/layouts/PageLayout.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './PageLayout.css';
 
+const NotifList = (props) => {
+  return (
+    <div id="notif">
+      <div id="notif-arrow"></div>
+      <div className="notif-element">
+        <div className="picture-notif-background" style={{ backgroundImage: "url(https://images.unsplash.com/photo-1520512202623-51c5c53957df?h=1000&q=10)" }}></div>
+        <div className="white-notif-background"></div>
+      </div>
+      <div className="notif-element">
+        <div className="picture-notif-background" style={{ backgroundImage: "url(https://images.unsplash.com/photo-1522234811749-abc512463137?h=1000&q=10)" }}></div>
+        <div className="white-notif-background"></div>
+      </div>
+    </div>
+  )
+}
+
 class PageLayout extends Component {
   render() {
     const {
@@ -16,6 +32,8 @@ class PageLayout extends Component {
               <a href='/browsing' className="logout"><span>Browsing</span></a>
               <a href='/matches' className="logout"><span>Matches</span></a>
               <a href='/profile' className="logout"><span>My Profile</span></a>
+              <span className="logout" style={{ cursor: "pointer" }}>Notifications</span>
+              <NotifList />
               <a href='/logout' className="logout"><span>Logout</span></a>
             </div>
           </div>
