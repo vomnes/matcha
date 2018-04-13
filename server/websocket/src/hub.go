1f9c8be70c3a6935cb057dec178385a2b871e354
Valentin OMNES <vomnes@student.42.fr>
2018-04-13 15:19:39 +0200

    feat: Start handle dispatch websockets

--- server/websocket/src/hub.go
+++ server/websocket/src/hub.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"../../lib"
 )
+
+type messageDecoded struct {
+	Event  string
+	Target string
+	Data   string
+}
 
 type message struct {
 	data     []byte
@@ -30,6 +38,67 @@ type Hub struct {
 	unregister chan subscription
 }
 
+func (h *Hub) toTarget(m message, targetUsername string) {
+	fmt.Printf("Targetuser: %s - Message: %s\n", targetUsername, string(m.data))
+	connections := h.users[targetUsername] // Get connections linked to this username
+	if connections != nil {                // Connection exists
+		for c := range connections {
+			select {
+			case c.send <- m.data: // Send data to this connection - Userid != current user
+			default:
+				close(c.send)
+				delete(connections, c)
+				if len(connections) == 0 {
+					delete(h.users, m.username)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) toEveryone(m message) {
+	fmt.Printf("Targetuser: Everyone - Message: %s\n", string(m.data))
+	for username, connections := range h.users {
+		if username == m.username { // Not send to your self
+			continue
+		}
+		for c := range connections {
+			select {
+			case c.send <- m.data:
+			default:
+				close(c.send)
+				delete(connections, c)
+				if len(connections) == 0 {
+					delete(h.users, m.username)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) dispatch(m message) {
+	var msgDecoded messageDecoded
+	err := lib.DecodeByte(m.data, &msgDecoded)
+	if err != nil {
+		fmt.Println("[WS] Failed to decode message", "-", err)
+		return
+	}
+	if msgDecoded.Target != "" && msgDecoded.Event != "login" && msgDecoded.Event != "logout" {
+		h.toTarget(m, msgDecoded.Target)
+	} else {
+		h.toEveryone(m)
+	}
+}
+
+func (h *Hub) handleLogout(username string, previousTime *time.Time) {
+	time.Sleep(500 * time.Millisecond)
+	if h.users[username] == nil && time.Now().Sub(*previousTime) > (500*time.Millisecond) {
+		*previousTime = time.Now()
+		fmt.Println("user loggued out")
+	}
+	fmt.Println(h.users[username])
+}
+
 func (h *Hub) run() {
 	var previousTime time.Time
 	for {
@@ -51,32 +120,11 @@ func (h *Hub) run() {
 					if len(connections) == 0 {
 						delete(h.users, s.username)
 					}
-					go func() { // Logout
-						time.Sleep(500 * time.Millisecond)
-						if h.users[s.username] == nil && time.Now().Sub(previousTime) > (500*time.Millisecond) {
-							previousTime = time.Now()
-							fmt.Println("user loggued out")
-						}
-						fmt.Println(h.users[s.username])
-					}()
+					go h.handleLogout(s.username, &previousTime) // username <> previousTime -> Error case too many
 				}
 			}
-		case m := <-h.broadcast:
-			fmt.Println(string(m.data))
-			connections := h.users[m.username] // Get connections linked to this username
-			if connections != nil {            // Connection exists
-				for c := range connections {
-					select {
-					case c.send <- m.data: // Send data to this connection - Userid != current user
-					default:
-						close(c.send)
-						delete(connections, c)
-						if len(connections) == 0 {
-							delete(h.users, m.username)
-						}
-					}
-				}
-			}
+		case m := <-h.broadcast: // Who will receive the message
+			h.dispatch(m)
 		}
 	}
 }
